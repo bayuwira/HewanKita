c65c8fc7dbb0a2660f3f5b9c39535fe7eebdb9b7
bayuwira <bayuwira1718@gmail.com>
2019-10-29 23:36:24 +0800

    nambah admin LTE

--- resources/views/adminpanel/partial/sidebar.blade.php
+++ resources/views/adminpanel/partial/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">INVENTION Udayana</span>
+        <span class="brand-text font-weight-light">HewanKita</span>
     </a>
 
     <div class="sidebar">
