4b46756461b22274e3a9b77f10fd163e7cd4e052
igstbagusdharmaputra <dharmatkjone@gmail.com>
2019-12-02 16:51:50 +0800

    checkroleaja

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123#'),
+            'hak_akses' => '1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        // DB::table('users')->insert([
+        //     'name' => 'test',
+        //     'email' => 'test@gmail.com',
+        //     'password' => bcrypt('test123#'),
+        //     'hak_akses' => '2',
+        //     'created_at' => date('Y-m-d H:i:s'),
+        //     'updated_at' => date('Y-m-d H:i:s')
+        // ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function(){
+Route::get('/', function () {
     return view('landing.home.index');
 })->name('landing.home.index');
 
 
-Route::group(['middleware' => ['auth']], function(){
-    $urlAdmin = '/control-panel';
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['checkroleuserlogin:1']], function () {
+        $urlAdmin = '/control-panel';
 
-    Route::get($urlAdmin, 'Admin\DashboardController@index')->name('adminpanel.dashboard.index');
-    
-    Route::resource($urlAdmin . '/produk', 'Admin\ProdukController')->names([
-        'index' => 'adminpanel.produk.index',
-        'create' => 'adminpanel.produk.create',
-        'store' => 'adminpanel.produk.store',
-        'edit' => 'adminpanel.produk.edit',
-        'update' => 'adminpanel.produk.update',
-        'destroy' => 'adminpanel.produk.destroy'
-    ])->except([
-        'show'
-    ]);
+        Route::get($urlAdmin, 'Admin\DashboardController@index')->name('adminpanel.dashboard.index');
 
-    Route::resource($urlAdmin . '/kategori/produk', 'Admin\ProdukKategoriController')->names([
-        'index' => 'adminpanel.kategori.produk.index',
-        'create' => 'adminpanel.kategori.produk.create',
-        'store' => 'adminpanel.kategori.produk.store',
-        'edit' => 'adminpanel.kategori.produk.edit',
-        'update' => 'adminpanel.kategori.produk.update',
-        'destroy' => 'adminpanel.kategori.produk.destroy'
-    ])->except([
-        'show'
-    ]);
+        Route::resource($urlAdmin . '/produk', 'Admin\ProdukController')->names([
+            'index' => 'adminpanel.produk.index',
+            'create' => 'adminpanel.produk.create',
+            'store' => 'adminpanel.produk.store',
+            'edit' => 'adminpanel.produk.edit',
+            'update' => 'adminpanel.produk.update',
+            'destroy' => 'adminpanel.produk.destroy'
+        ])->except([
+            'show'
+        ]);
+
+        Route::resource($urlAdmin . '/kategori/produk', 'Admin\ProdukKategoriController')->names([
+            'index' => 'adminpanel.kategori.produk.index',
+            'create' => 'adminpanel.kategori.produk.create',
+            'store' => 'adminpanel.kategori.produk.store',
+            'edit' => 'adminpanel.kategori.produk.edit',
+            'update' => 'adminpanel.kategori.produk.update',
+            'destroy' => 'adminpanel.kategori.produk.destroy'
+        ])->except([
+            'show'
+        ]);
+    });
 });
 
 Auth::routes();
